14f599ea295cb165db422b56f51bb9dff73ed1e9
Alexander Ferreira <alexander.ferreira1@hotmail.com>
2024-05-21 22:15:07 -0300

    feat(Jokenpo): initial structure for Jokenpo project app

---
+++ Jokenpo/app/src/main/java/br/com/alexander/jokenpo/MainActivity.kt
@@ -0,0 +1,20 @@
+package br.com.alexander.jokenpo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.com.alexander.jokenpo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+
+    }
+}
